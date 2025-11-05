8883a23f5857cc96c02cb8f5d5aec1a151d3496a
Adhara <adhara0620@gmail.com>
2025-11-05 13:17:19 -0700

    Ajuste de clases de diseño

--- vistas/ciclosJavascript.php
+++ vistas/ciclosJavascript.php
@@ -2,34 +2,88 @@
 include('../includes/header.php');
 include('../includes/menu.php');
 ?>
+
 <!-- Asi deben quedar todos los encabezados -->
 <div class="pt-20 px-6">
   <h1 class="titulo text-3xl text-[#26408b] font-semibold">
-    Ejercicios De Repetición JavaScript
+    Estructuras de Repetición JavaScript
   </h1>
   <hr class="linea-separadora">
-  <!-- contenido -->
-  <div class="tarjeta-contenedor">
+  <!-- For -->
+  <h2 class="subTitulo text-3xl text-[#0d0221] font-semibold">Ejercicios For</h2>
+  <!-- resto del contenido -->
+   <div class="tarjeta-contenedor">
   <div class="tarjeta">
-    <p></p>
+    <p>El aprendizaje constante impulsa la creatividad y fortalece la mente.
+       Cada proyecto representa una oportunidad para explorar nuevas ideas, 
+       mejorar habilidades y descubrir caminos inesperados. La paciencia, el 
+       esfuerzo y la curiosidad son las claves del crecimiento personal y profesional.</p>
     <br>
     <button class="buttonEjecutar button:hover">Ejecutar</button>
   </div>
   <div class="tarjeta">
-    <p></p>
+    <p>El aprendizaje constante impulsa la creatividad y fortalece la mente.
+       Cada proyecto representa una oportunidad para explorar nuevas ideas, 
+       mejorar habilidades y descubrir caminos inesperados. La paciencia, el 
+       esfuerzo y la curiosidad son las claves del crecimiento personal y profesional.</p>
     <br>
     <button class="buttonEjecutar button:hover">Ejecutar</button>
   </div>
   <div class="tarjeta">
-    <p></p>
+    <p>El aprendizaje constante impulsa la creatividad y fortalece la mente.
+       Cada proyecto representa una oportunidad para explorar nuevas ideas, 
+       mejorar habilidades y descubrir caminos inesperados. La paciencia, el 
+       esfuerzo y la curiosidad son las claves del crecimiento personal y profesional.</p>
+      <br>
+       <button class="buttonEjecutar button:hover">Ejecutar</button>
+  </div>
+  <div class="tarjeta">
+    <p>El aprendizaje constante impulsa la creatividad y fortalece la mente.
+       Cada proyecto representa una oportunidad para explorar nuevas ideas, 
+       mejorar habilidades y descubrir caminos inesperados. La paciencia, el 
+       esfuerzo y la curiosidad son las claves del crecimiento personal y profesional.</p>
+    <br>
+    <button class="buttonEjecutar button:hover">Ejecutar</button>
+  </div>
+  
+</div>
+<hr class="linea-separadora2">
+<!-- While -->
+   <h2 class="subTitulo text-3xl text-[#0d0221] font-semibold">Ejercicios For</h2>
+    <div class="tarjeta-contenedor">
+  <div class="tarjeta">
+    <p>El aprendizaje constante impulsa la creatividad y fortalece la mente.
+       Cada proyecto representa una oportunidad para explorar nuevas ideas, 
+       mejorar habilidades y descubrir caminos inesperados. La paciencia, el 
+       esfuerzo y la curiosidad son las claves del crecimiento personal y profesional.</p>
     <br>
     <button class="buttonEjecutar button:hover">Ejecutar</button>
   </div>
   <div class="tarjeta">
-    <p></p>
+    <p>El aprendizaje constante impulsa la creatividad y fortalece la mente.
+       Cada proyecto representa una oportunidad para explorar nuevas ideas, 
+       mejorar habilidades y descubrir caminos inesperados. La paciencia, el 
+       esfuerzo y la curiosidad son las claves del crecimiento personal y profesional.</p>
     <br>
     <button class="buttonEjecutar button:hover">Ejecutar</button>
   </div>
+  <div class="tarjeta">
+    <p>El aprendizaje constante impulsa la creatividad y fortalece la mente.
+       Cada proyecto representa una oportunidad para explorar nuevas ideas, 
+       mejorar habilidades y descubrir caminos inesperados. La paciencia, el 
+       esfuerzo y la curiosidad son las claves del crecimiento personal y profesional.</p>
+      <br>
+       <button class="buttonEjecutar button:hover">Ejecutar</button>
+  </div>
+  <div class="tarjeta">
+    <p>El aprendizaje constante impulsa la creatividad y fortalece la mente.
+       Cada proyecto representa una oportunidad para explorar nuevas ideas, 
+       mejorar habilidades y descubrir caminos inesperados. La paciencia, el 
+       esfuerzo y la curiosidad son las claves del crecimiento personal y profesional.</p>
+    <br>
+    <button class="buttonEjecutar button:hover">Ejecutar</button>
+  </div>
+  
 </div>
 </div>
 <?php
